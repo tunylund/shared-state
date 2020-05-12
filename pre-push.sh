cf8a42d35c55f2a9ca3e49571ad36ffb6a9d2fdd
Tuomas Nylund <tunylund@gmail.com>
2020-05-12 15:49:37 +0100

    update github hooks to match monorepo structure

--- pre-push.sh
+++ pre-push.sh
@@ -3,16 +3,22 @@ set -e
 
 (
     cd client
+    [ -d node_modules ] && npm i
+    [ -d node_modules ] || npm ci
     npm run build
     npm run test
 )
 (
     cd server
+    [ -d node_modules ] && npm i
+    [ -d node_modules ] || npm ci
     npm run build
     npm run test
 )
 (
     cd acceptance-tests
+    [ -d node_modules ] && npm i
+    [ -d node_modules ] || npm ci
     npm run test
 )
 
