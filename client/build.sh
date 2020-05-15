ee3ce4afec72d301d4365424d993c7517f48b1fb
Tuomas Nylund <tunylund@gmail.com>
2020-05-15 11:35:24 +0100

    try to support both mac and linux for the build scripts

--- client/build.sh
+++ client/build.sh
@@ -3,15 +3,19 @@ set -e
 
 ./node_modules/.bin/tsc
 
-which sed
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  SED="sed -i ''"
+else
+  SED="sed -i"
+fi
 
 for f in `find dist -name '*.js'`; do
   target=`echo $f | sed -e 's/\\.js/\\.mjs/'`
   echo "converting $f to $target"
   cp $f $target
-  sed -i '' -e "s/from ['\"]\\.\\(.*\\)['\"]/from '\\.\\1.mjs'/g" $target
+  $SED -e "s/from ['\"]\\.\\(.*\\)['\"]/from '\\.\\1.mjs'/g" $target
 
   cp "$f.map" "$target.map"
-  sed -i '' "s/\\.js\\.map/\\.mjs\\.map/g" $target
-  sed -i '' "s/\\.js/\\.mjs/g" "$target.map"
+  $SED "s/\\.js\\.map/\\.mjs\\.map/g" $target
+  $SED "s/\\.js/\\.mjs/g" "$target.map"
 done
--- server/build.sh
+++ server/build.sh
@@ -3,6 +3,12 @@ set -e
 
 ./node_modules/.bin/tsc -p tsconfig.notests.json
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  SED="sed -i ''"
+else
+  SED="sed -i"
+fi
+
 for f in `find dist -name '*.js'`; do
-  sed -i '' -e "s/from ['\"]\\.\\(.*\\)['\"]/from '\\.\\1.js'/g" $f
+  $SED -e "s/from ['\"]\\.\\(.*\\)['\"]/from '\\.\\1.js'/g" $f
 done
